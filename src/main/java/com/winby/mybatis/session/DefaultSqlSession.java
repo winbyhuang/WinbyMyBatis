a868b1baecec9e6ebbd32a6ee2e725590bbeb0b2
winby <winbywenbo@gmail.com>
2019-12-17 14:05:12 +0800

    init

---
+++ src/main/java/com/winby/mybatis/session/DefaultSqlSession.java
@@ -0,0 +1,61 @@
+package com.winby.mybatis.session;
+
+import java.lang.reflect.Proxy;
+import java.util.List;
+
+import com.winby.mybatis.binding.MapperProxy;
+import com.winby.mybatis.config.Configuration;
+import com.winby.mybatis.config.MappedStatement;
+import com.winby.mybatis.excutor.DefaultExecutor;
+import com.winby.mybatis.excutor.Executor;
+
+/**
+ * @author winby
+ * 
+ * mybaitis暴露给外部的接口，实现增删改查的能力
+ * 
+ * 1.对外提供数据访问的api
+ * 2.对内将请求转发给executor
+ * 3.？
+ *
+ */
+public class DefaultSqlSession implements SqlSession {
+	
+	private final Configuration conf;
+	
+	private Executor executor;
+	
+	
+
+	public DefaultSqlSession(Configuration conf) {
+		super();
+		this.conf = conf;
+		executor = new DefaultExecutor(conf);
+	}
+
+	@Override
+	public <T> T selectOne(String statement, Object parameter) {
+		List<Object> selectList = this.selectList(statement, parameter);
+		if(selectList == null || selectList.size()==0){
+			return null;
+		}
+		if(selectList.size()==1){
+			return (T) selectList.get(0);
+		}else{
+			throw new RuntimeException("Too Many Results!");
+		}
+	}
+	
+	@Override
+	public <E> List<E> selectList(String statement, Object parameter) {
+		MappedStatement ms = conf.getMappedStatements().get(statement);
+		return executor.query(ms, parameter);
+	}
+
+	@Override
+	public <T> T getMapper(Class<T> type) {
+		MapperProxy mp = new MapperProxy(this);
+		return (T) Proxy.newProxyInstance(type.getClassLoader(), new Class[]{type}, mp);
+	}
+
+}
